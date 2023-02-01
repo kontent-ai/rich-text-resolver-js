f64659191654d4500f5902ddb59a56f37e84da06
Ondřej Chrastina <ondrejch@kontent.ai>
2023-02-01 14:47:00 +0100

    format interface

--- src/IRichTextInput.ts
+++ src/IRichTextInput.ts
@@ -2,17 +2,21 @@
 
 export type IRichTextInput = {
     value: string,
-    images: {[key: string]: {
-        image_id: string,
-        description: string,
-        url: string,
-        width: number,
-        height: number
-    }},
-    links: {[key: string]: {
-        codename: string,
-        type: string,
-        url_slug: string
-    }},
+    images: {
+        [key: string]: {
+            image_id: string,
+            description: string | null,
+            url: string,
+            width: number | undefined,
+            height: number | undefined
+        }
+    },
+    links: {
+        [key: string]: {
+            codename: string,
+            type: string,
+            url_slug: string
+        }
+    },
     modular_content: string[]
 }
