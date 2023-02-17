0c4db848230128c80902a5098f8fed6109e000ec
Daniel Pokorny <danielp@kontent.ai>
2023-02-17 11:41:00 +0100

    export missing type definitions

--- index.ts
+++ index.ts
@@ -1,1 +1,1 @@
-export { RichTextBrowserParser, RichTextNodeParser, isElement, isUnPairedElement, isImage, isLinkedItem, isItemLink, IDomHtmlNode, IDomNode, IDomTextNode } from "./src";
+export { RichTextBrowserParser, RichTextNodeParser, isElement, isText, isUnPairedElement, isImage, isLinkedItem, isItemLink, IDomHtmlNode, IDomNode, IDomTextNode } from "./src";
