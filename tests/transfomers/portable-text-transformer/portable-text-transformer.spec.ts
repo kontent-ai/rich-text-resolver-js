ef7388bf7aa23a459bac7adf0b29d9e8fa744850
Ondřej Chrastina <ondrejch@kontent.ai>
2023-08-29 12:50:15 +0200

    avoid code duplication

--- tests/transfomers/portable-text-transformer/portable-text-transformer.spec.ts
+++ tests/transfomers/portable-text-transformer/portable-text-transformer.spec.ts
@@ -1,4 +1,4 @@
-import { browserParse, nodeParse, transformToPortableText } from "../../../src";
+import { IPortableTextItem, browserParse, nodeParse, transformToPortableText } from "../../../src";
 
 jest.mock('short-unique-id', () => {
   return jest.fn().mockImplementation(() => {
@@ -7,12 +7,19 @@ jest.mock('short-unique-id', () => {
 });
 
 describe("portable text transformer", () => {
-  it("transforms empty rich text", () => {
-    const input = "<p><br></p>";
+
+  const transformInput = (input: string): { nodeResult: IPortableTextItem[], browserResult: IPortableTextItem[] } => {
     const browserTree = browserParse(input);
     const nodeTree = nodeParse(input);
-    const nodeResult = transformToPortableText(nodeTree);
-    const browserResult = transformToPortableText(browserTree);
+    return {
+      nodeResult: transformToPortableText(nodeTree),
+      browserResult: transformToPortableText(browserTree)
+    };
+  }
+
+  it("transforms empty rich text", () => {
+    const input = "<p><br></p>";
+    const { nodeResult, browserResult } = transformInput(input);
 
     expect(nodeResult).toMatchSnapshot();
     expect(nodeResult).toMatchObject(browserResult);
@@ -186,10 +193,7 @@ describe("portable text transformer", () => {
       </tbody>
     </table>`
   ])("transforms table with input %s", (input) => {
-    const browserTree = browserParse(input);
-    const nodeTree = nodeParse(input);
-    const nodeResult = transformToPortableText(nodeTree);
-    const browserResult = transformToPortableText(browserTree);
+    const { nodeResult, browserResult } = transformInput(input);
 
     expect(nodeResult).toMatchSnapshot();
     expect(nodeResult).toMatchObject(browserResult);
@@ -198,10 +202,7 @@ describe("portable text transformer", () => {
   it("transforms item links", () => {
     const input = `<p><a data-item-id="23f71096-fa89-4f59-a3f9-970e970944ec" href="">text<strong>link to an item</strong></a></p>`;
 
-    const browserTree = browserParse(input);
-    const nodeTree = nodeParse(input);
-    const nodeResult = transformToPortableText(nodeTree);
-    const browserResult = transformToPortableText(browserTree);
+    const { nodeResult, browserResult } = transformInput(input);
 
     expect(nodeResult).toMatchSnapshot();
     expect(nodeResult).toMatchObject(browserResult);
@@ -210,10 +211,7 @@ describe("portable text transformer", () => {
 
   it("transforms external links", () => {
     const input = `<h2><strong>Kontent supports portable text!</strong></h2>\n<p>For more information, check out the related <a href="https://github.com/portabletext/portabletext" data-new-window="true" title="Portable text repo" target="_blank" rel="noopener noreferrer"><strong>GitHub repository.</strong></a></p>`
-    const browserTree = browserParse(input);
-    const nodeTree = nodeParse(input);
-    const nodeResult = transformToPortableText(nodeTree);
-    const browserResult = transformToPortableText(browserTree);
+    const { nodeResult, browserResult } = transformInput(input);
 
     expect(nodeResult).toMatchSnapshot();
     expect(nodeResult).toMatchObject(browserResult);
@@ -221,10 +219,7 @@ describe("portable text transformer", () => {
 
   it("transforms nested styles", () => {
     const input = `<p><strong>all text is bold and last part is </strong><em><strong>also italic and this is also </strong></em><em><strong><sup>superscript</sup></strong></em></p>`
-    const browserTree = browserParse(input);
-    const nodeTree = nodeParse(input);
-    const nodeResult = transformToPortableText(nodeTree);
-    const browserResult = transformToPortableText(browserTree);
+    const { nodeResult, browserResult } = transformInput(input);
 
     expect(nodeResult).toMatchSnapshot();
     expect(nodeResult).toMatchObject(browserResult);
@@ -232,10 +227,7 @@ describe("portable text transformer", () => {
 
   it("transforms lists", () => {
     const input = `<ul><li>first level bullet</li><li>first level bullet</li><ol><li>nested number in bullet list</li></ol></ul><ol><li>first level item</li><li>first level item</li><ol><li>second level item</li><li><strong>second level item </strong></li></ol>`;
-    const browserTree = browserParse(input);
-    const nodeTree = nodeParse(input);
-    const nodeResult = transformToPortableText(nodeTree);
-    const browserResult = transformToPortableText(browserTree);
+    const { nodeResult, browserResult } = transformInput(input);
 
     expect(nodeResult).toMatchSnapshot();
     expect(nodeResult).toMatchObject(browserResult);
@@ -243,10 +235,7 @@ describe("portable text transformer", () => {
 
   it("transforms images", () => {
     const input = `<figure data-asset-id="7d866175-d3db-4a02-b0eb-891fb06b6ab0" data-image-id="7d866175-d3db-4a02-b0eb-891fb06b6ab0"><img src="https://assets-eu-01.kc-usercontent.com:443/6d864951-9d19-0138-e14d-98ba886a4410/236ecb7f-41e3-40c7-b0db-ea9c2c44003b/sharad-bhat-62p19OGT2qg-unsplash.jpg" data-asset-id="7d866175-d3db-4a02-b0eb-891fb06b6ab0" data-image-id="7d866175-d3db-4a02-b0eb-891fb06b6ab0" alt=""></figure><p><em>text in a paragraph</em></p>`;
-    const browserTree = browserParse(input);
-    const nodeTree = nodeParse(input);
-    const nodeResult = transformToPortableText(nodeTree);
-    const browserResult = transformToPortableText(browserTree);
+    const { nodeResult, browserResult } = transformInput(input);
 
     expect(nodeResult).toMatchSnapshot();
     expect(nodeResult).toMatchObject(browserResult);
@@ -254,20 +243,14 @@ describe("portable text transformer", () => {
 
   it("transforms complex rich text into portable text", () => {
     const input = `<table><tbody><tr><td><ul><li>list item</li><ol><li>nested list item</li></ol></ul></td></tr></tbody></table><table><tbody>\n  <tr><td><p>paragraph 1</p><p>paragraph 2</p></td><td><ul>\n  <li>list item\n     </li>\n</ul>\n</td><td><a href="http://google.com" data-new-window="true" title="linktitle" target="_blank" rel="noopener noreferrer">this is a<strong>strong</strong>link</a></td></tr>\n<tr><td><h1><strong>nadpis</strong></h1></td><td><p>text</p></td><td><p>text</p></td></tr>\n<tr><td><em>italic text</em></td><td><p>text</p></td><td><p>text</p></td></tr>\n</tbody></table><p>text<a href="http://google.com" data-new-window="true" title="linktitle" target="_blank" rel="noopener noreferrer">normal and<strong>bold</strong>link</a></p><h1>heading</h1><object type="application/kenticocloud" data-type="item" data-rel="link" data-codename="test_item"></object>`;
-    const browserTree = browserParse(input);
-    const nodeTree = nodeParse(input);
-    const nodeResult = transformToPortableText(nodeTree);
-    const browserResult = transformToPortableText(browserTree);
+    const { nodeResult, browserResult } = transformInput(input);
 
     expect(nodeResult).toMatchSnapshot();
     expect(nodeResult).toMatchObject(browserResult);
   })
   it("transforms linked items/components", () => {
     const input = `<object type="application/kenticocloud" data-type="item" data-rel="link" data-codename="test_item"></object>`;
-    const browserTree = browserParse(input);
-    const nodeTree = nodeParse(input);
-    const nodeResult = transformToPortableText(nodeTree);
-    const browserResult = transformToPortableText(browserTree);
+    const { nodeResult, browserResult } = transformInput(input);
 
     expect(nodeResult).toMatchSnapshot();
     expect(nodeResult).toMatchObject(browserResult);
@@ -275,10 +258,7 @@ describe("portable text transformer", () => {
 
   it("doesn't create duplicates for nested spans", () => {
     const input = `<p>text<strong>bold</strong></p>`;
-    const browserTree = browserParse(input);
-    const nodeTree = nodeParse(input);
-    const nodeResult = transformToPortableText(nodeTree);
-    const browserResult = transformToPortableText(browserTree);
+    const { nodeResult, browserResult } = transformInput(input);
 
     expect(nodeResult).toMatchSnapshot();
     expect(nodeResult).toMatchObject(browserResult);
@@ -295,10 +275,7 @@ describe("portable text transformer", () => {
 
   it("doesn't extend link mark to adjacent spans", () => {
     const input = `<p>The coffee drinking culture has been evolving for hundreds and thousands of years. It has never been <a data-item-id="3120ec15-a4a2-47ec-8ccd-c85ac8ac5ba5" href="">so rich as <strong>today</strong></a>. How do you make sense of different types of coffee, what do the particular names in beverage menus mean and what beverage to choose for which occasion in your favorite café?</p>`
-    const browserTree = browserParse(input);
-    const nodeTree = nodeParse(input);
-    const nodeResult = transformToPortableText(nodeTree);
-    const browserResult = transformToPortableText(browserTree);
+    const { nodeResult, browserResult } = transformInput(input);
 
     expect(nodeResult).toMatchSnapshot();
     expect(nodeResult).toMatchObject(browserResult);
@@ -325,10 +302,7 @@ describe("portable text transformer", () => {
    </ul>
 </li>
 </ul>`
-    const browserTree = browserParse(input);
-    const nodeTree = nodeParse(input);
-    const nodeResult = transformToPortableText(nodeTree);
-    const browserResult = transformToPortableText(browserTree);
+    const { nodeResult, browserResult } = transformInput(input);
 
     expect(nodeResult).toMatchSnapshot();
     expect(nodeResult).toMatchObject(browserResult);
@@ -343,10 +317,7 @@ describe("portable text transformer", () => {
         </tr>
       </tbody>
     </table>`;
-    const browserTree = browserParse(input);
-    const nodeTree = nodeParse(input);
-    const nodeResult = transformToPortableText(nodeTree);
-    const browserResult = transformToPortableText(browserTree);
+    const { nodeResult, browserResult } = transformInput(input);
 
     expect(nodeResult).toMatchSnapshot();
     expect(nodeResult).toMatchObject(browserResult);
