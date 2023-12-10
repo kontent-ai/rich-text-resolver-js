70509575a06697b00e634b9b9cf4211883d5210a
Daniel Pokorny <danielp@kontent.ai>
2023-12-11 00:19:56 +0100

    add default image resolution and tests
    add resolverMethod type
    
    split resolution methods to standalone files
    
    extend default resolution for vue

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 export { transformToJson } from "./src/transformers/json-transformer/json-transformer.js";
 export { transformToPortableText } from "./src/transformers/portable-text-transformer/portable-text-transformer.js";
-export { resolveTable, traversePortableText } from "./src/utils/transformer-utils.js";
+export { extendPortableText } from "./src/utils/transformer-utils.js";
 export { parse as browserParse } from "./src/parser/browser/rich-text-browser-parser.js";
 export { parse as nodeParse } from "./src/parser/node/rich-text-node-parser.js";
---
+++ src/utils/resolution/vue.ts
@@ -0,0 +1,67 @@
+import { PortableTextBlock } from "@portabletext/types";
+
+import {
+  PortableTextImage,
+  PortableTextTable,
+  PortableTextTableCell,
+  PortableTextTableRow,
+} from "../../index.js";
+
+/**
+ * Renders a portable text table to a Vue virtual DOM node.
+ *
+ * @param {PortableTextTable} table - The portable text table object to render.
+ * @param {(value: PortableTextBlock[]) => string} resolver - A function that resolves
+ *        the content of each cell in the table.
+ * @param {Function} vueRenderFunction - A Vue render function, typically the `h` function from Vue.
+ * @returns {VueNode} The rendered table as a Vue virtual DOM node.
+ */
+export const resolveTable = (
+  table: PortableTextTable,
+  vueRenderFunction: Function,
+  resolver: (value: PortableTextBlock[]) => string
+) => {
+  const renderCell = (cell: PortableTextTableCell) => {
+    const cellContent = resolver(cell.content);
+    return vueRenderFunction("td", {}, cellContent);
+  };
+
+  const renderRow = (row: PortableTextTableRow) => {
+    const cells = row.cells.map(renderCell);
+    return vueRenderFunction("tr", {}, cells);
+  };
+
+  const renderRows = () => table.rows.map(renderRow);
+
+  return vueRenderFunction("table", {}, renderRows());
+};
+
+/**
+ * Resolves an image object to a Vue virtual DOM node using a provided Vue render function.
+ *
+ * @param {PortableTextImage} image - The portable text image object to be rendered.
+ * @param {Function} vueRenderFunction - A Vue render function, typically the `h` function from Vue.
+ * @param {(image: PortableTextImage) => VueImage} resolver - A function that takes an image object
+ *        and returns an object with `src` and `alt` properties, and possibly other HTML attributes.
+ * @returns {VueNode} The resolved image as a Vue virtual DOM node.
+ */
+export const resolveImage = (
+  image: PortableTextImage,
+  vueRenderFunction: Function,
+  resolver: (image: PortableTextImage) => VueImage
+) => vueRenderFunction("img", resolver(image));
+
+/**
+ * Provides a default resolver function for an image object to Vue. This function can be used as
+ * a default argument for the `resolveImage` function.
+ *
+ * @param {PortableTextImage} image - The portable text image object to be rendered.
+ * @returns {VueImage} An object representing the image, containing `src` and `alt` properties,
+ *          and potentially other HTML attributes.
+ */
+export const toVueImageDefault = (image: PortableTextImage): VueImage => ({
+  src: image.asset.url,
+  alt: image.asset.alt || "",
+});
+
+type VueImage = { src: string; alt: string; [key: string]: any };
