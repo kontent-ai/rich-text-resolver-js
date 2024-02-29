6eab0f2a6b0ee23344b5d9a5f975795d7c69a08c
Daniel Pokorny <danielp@kontent.ai>
2024-02-29 13:33:43 +0100

    add entry points to package.json, adjust root exports

--- index.ts
+++ index.ts
@@ -3,3 +3,8 @@ export { transformToJson } from "./src/transformers/json-transformer/json-transf
 export { transformToPortableText } from "./src/transformers/portable-text-transformer/portable-text-transformer.js";
 export { parse as browserParse } from "./src/parser/browser/rich-text-browser-parser.js";
 export { parse as nodeParse } from "./src/parser/node/rich-text-node-parser.js";
+export { resolveImage, resolveTable, toHTMLImageDefault } from "./src/utils/resolution/html.js";
+export { resolveImage as resolveImageVue, resolveTable as resolveTableVue, toVueImageDefault } from "./src/utils/resolution/vue.js";
+export * from './src/transformers/index.js';
+export * from './src/parser/parser-models.js';
+export * from './src/utils/common-utils.js';
