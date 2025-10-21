ce8ac76c8cbad81b2b231efd9156071c7727fdce
Ivan Kiral <kiralivan@gmail.com>
2025-10-21 14:29:07 +0200

    make rich-text-resolver-html package

---
+++ packages/rich-text-resolver-html/src/index.ts
@@ -0,0 +1,10 @@
+// biome-ignore lint/performance/noBarrelFile: public barrel
+export {
+  defaultComponents,
+  type PortableTextHtmlResolvers,
+  resolveImage,
+  resolveTable,
+  toHTML,
+  toHTMLImageDefault,
+} from "./html.js";
+export { toManagementApiFormat } from "./mapi.js";
